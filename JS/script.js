54a66549592e19b63456596a2b23c63e418fe06b
AhmadSaleh22 <dev.ahmadsaleh@gmail.com>
2021-01-26 19:41:01 +0200

    Adding Js code into index file

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+function moveSignup(){
+    window.location.href="pages/Signup.html";
+}
+function moveLogin(){
+    window.location.href="pages/login.html";
+}
+
+const users = [
+        {name:"",username:"",password:""},
+    ]
